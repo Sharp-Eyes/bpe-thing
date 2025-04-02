c5542d3c5b0dcf6d1e099c3cb18a9a05f59d3989
Chromosomologist <chromosomologist@gmail.com>
2025-04-02 23:34:46 +0200

    feat: add cli --help flag

--- src/main.rs
+++ src/main.rs
@@ -115,17 +115,25 @@ fn generate_from_seed<P: AsRef<Path>>(
 }
 
 macro_rules! pos_args {
-    ($iter:ident, $($n:literal),*) => {
+    ($cmd:literal, $iter:ident, $($n:literal),*) => {
         (
             $(
-                $iter.next().context(format!("Please provide a value for '{}'.", $n))?,
+                {
+                    let arg = $iter.next().context(format!("Please provide a value for '{}'.", $n))?;
+                    if arg.as_str() == "--help" || arg.as_str() == "-h" {
+                        print_help(Some($cmd));
+                        return Ok(());
+                    } else {
+                        arg
+                    }
+                },
             )*
         )
     };
 }
 
 macro_rules! flag_args {
-    ($iter:ident, $(($i:ident, $p:pat)),*) => {
+    ($cmd:literal, $iter:ident, $(($i:ident, $p:pat)),*) => {
         while let Some(arg) = $iter.next() {
             match arg.as_ref() {
                 $(
@@ -134,43 +142,95 @@ macro_rules! flag_args {
                             .next()
                             .context(format!("Please provide a value for flag '{}'", arg))?
                             .parse()?
-                    }
+                    },
                 )*
+                "--help" | "-h" => {
+                    print_help(Some($cmd));
+                    return Ok(());
+                },
                 unknown => anyhow::bail!("Unknown flag: {}.", unknown),
             }
         }
     };
 }
 
+fn print_help(command: Option<&str>) {
+    match command {
+        Some("generate") => {
+            println!("Usage: bpe-thing generate <SEED> <BPE_PATH> [OPTIONS]");
+            println!("");
+            println!("Arguments:");
+            println!("  SEED      The seed from which to generate new text");
+            println!("  BPE_PATH  The path to a file that contains BPE grammar");
+            println!("");
+            println!("Options:");
+            println!("  -h, --help        Display concise help for this command");
+            println!("  -t, --max-tokens  The maximum number of tokens to generate");
+            println!("");
+        }
+        Some("parse") => {
+            println!("Usage: bpe-thing parse <TXT_PATH> <BPE_PATH> [OPTIONS]");
+            println!("");
+            println!("Arguments:");
+            println!("  TXT_PATH  The path to the sauce");
+            println!("  BPE_PATH  The path to the output BPE grammar file");
+            println!("");
+            println!("Options:");
+            println!("  -h, --help        Display concise help for this command");
+            println!("  -t, --max-tokens  The maximum number of tokens to use for the BPE grammar");
+            println!("");
+        }
+        _ => {
+            println!("Usage: bpe-thing [COMMAND] [OPTIONS]");
+            println!("");
+            println!("Commands:");
+            println!("  generate  Generate text from a seed and a BPE file");
+            println!("  parse     Parse a text file into a BPE file");
+            println!("");
+            println!("Options:");
+            println!("  -h, --help  Display concise help for this command");
+            println!("");
+        }
+    }
+}
+
 fn main() -> Result<()> {
-    let mut args_iter = env::args().skip(1);
+    let mut args_iter = env::args().peekable().skip(1);
     match args_iter.next() {
         Some(cmd) => match cmd.to_lowercase().as_ref() {
             "generate" | "gen" | "g" => {
-                // TODO: allow for --help here?
-                let (seed, bpe_path) = pos_args!(args_iter, "seed", "bpe_path");
+                let (seed, bpe_path) = pos_args!("generate", args_iter, "seed", "bpe_path");
 
                 let mut max_token_count = 32;
-                flag_args!(args_iter, (max_token_count, "--max-tokens" | "-t"));
+                flag_args!(
+                    "generate",
+                    args_iter,
+                    (max_token_count, "--max-tokens" | "-t")
+                );
 
                 println!("{}", generate_from_seed(seed, bpe_path, max_token_count)?);
             }
             "parse" | "p" => {
-                // TODO: allow for --help here?
-                let (txt_path, bpe_path) = pos_args!(args_iter, "txt_path", "bpe_path");
+                let (txt_path, bpe_path) = pos_args!("parse", args_iter, "txt_path", "bpe_path");
 
                 let mut max_token_count = u32::MAX;
-                flag_args!(args_iter, (max_token_count, "--max-tokens" | "-t"));
+                flag_args!("parse", args_iter, (max_token_count, "--max-tokens" | "-t"));
 
                 parse_bpe(txt_path, bpe_path, max_token_count)?;
                 return Ok(());
             }
-            "--help" | "-h" => {
-                todo!("help")
+            "--help" | "-h" => print_help(None),
+            unknown => {
+                println!("error: unrecognized subcommand '{}'", unknown);
+                println!("");
+                print_help(None);
             }
-            _ => todo!("invalid command + help"),
         },
-        None => todo!("help"),
+        None => {
+            println!("A weird little BPE implementation in Rust.");
+            println!("");
+            print_help(None)
+        }
     };
 
     Ok(())
