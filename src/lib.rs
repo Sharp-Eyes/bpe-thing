f7dca6a2fc2f9a3d11e96ef381e3fd5b506b54c0
Chromosomologist <chromosomologist@gmail.com>
2025-04-03 00:05:41 +0200

    feat: implement token debug command

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
 use anyhow::{Context, Result};
 use rand::{distr::weighted::WeightedIndex, prelude::*};
 use std::{
+    collections::HashMap,
     fmt::Display,
-    fs::File,
+    fs::{self, File},
     io::{Read, Write},
     path::Path,
 };
@@ -200,4 +201,123 @@ pub fn tokenize(string: String, token_grammar: &TokenGrammar) -> Vec<Token> {
     }
 
     tokens
+}
+
+/// Parse plaintext from a file into a bpe and store it to another file.
+pub fn parse_bpe<P: AsRef<Path>>(
+    txt_path: P,
+    bpe_path: P,
+    max_token_count: u32,
+) -> Result<TokenGrammar> {
+    let data = fs::read_to_string(txt_path)?;
+
+    // Byte to use for non-char BPE tokens, incremented on demand.
+    let mut char_count = 0;
+
+    // Input tokens to be encoded with BPE.
+    let mut tokens: Vec<Token> = data.chars().map(|c| Token::Char(c)).collect();
+
+    // Finalised token 'grammar' definition of (left-token, right-token, frequency).
+    let mut token_grammar: TokenGrammar = vec![];
+
+    // Hashmap of {(left-token, right-token): frequency} for the entire token stream.
+    let mut freqs: HashMap<(Token, Token), usize> = HashMap::new();
+    for i in 0..tokens.len() - 1 {
+        freqs
+            .entry((tokens[i].clone(), tokens[i + 1].clone()))
+            .and_modify(|freq| *freq += 1)
+            .or_insert(1);
+    }
+
+    for _ in 0..max_token_count {
+        let (max_pair, max_freq) = freqs
+            .iter()
+            .max_by(|(_, l_freq), (_, r_freq)| l_freq.cmp(r_freq))
+            .map(|(k, v)| ((*k).clone(), *v))
+            .unwrap();
+
+        if max_freq == 1 {
+            break;
+        };
+
+        freqs.remove(&max_pair);
+        token_grammar.push((max_pair.0.clone(), max_pair.1.clone(), max_freq as u32));
+
+        let mut i = 0;
+        while i < tokens.len() - 1 {
+            if max_pair.0 == tokens[i] && max_pair.1 == tokens[i + 1] {
+                // Replace most common pair bc at index i with new token Z: abcd -> aZd
+                tokens.push(Token::Pair(char_count)); // abcdZ
+                tokens.remove(i); // acdZ
+                tokens.swap_remove(i); // aZd
+
+                if i > 0 {
+                    // Decrement ac freq, increment aZ freq or create it if it does not exist.
+                    freqs
+                        .entry((tokens[i - 1].clone(), max_pair.0.clone()))
+                        .and_modify(|freq| *freq -= 1);
+                    freqs
+                        .entry((tokens[i - 1].clone(), Token::Pair(char_count)))
+                        .and_modify(|freq| *freq += 1)
+                        .or_insert(1);
+                }
+
+                if i < tokens.len() - 2 {
+                    // Decrement cd freq, increment Zd freq or create it if it does not exist.
+                    freqs
+                        .entry((max_pair.1.clone(), tokens[i + 1].clone()))
+                        .and_modify(|freq| *freq -= 1);
+                    freqs
+                        .entry((Token::Pair(char_count), tokens[i + 1].clone()))
+                        .and_modify(|freq| *freq += 1)
+                        .or_insert(1);
+                }
+            }
+
+            i += 1;
+        }
+
+        char_count += 1;
+    }
+
+    write_tokens(&token_grammar, bpe_path)?;
+
+    Ok(token_grammar)
+}
+
+/// Generate a string of gibberish from a seed and a path to a bpe file.
+pub fn generate_from_seed<P: AsRef<Path>>(
+    seed: String,
+    bpe_path: P,
+    max_token_count: u32,
+) -> Result<String> {
+    let token_grammar = load_tokens(bpe_path)?;
+
+    let mut seed_token_iter = tokenize(seed, &token_grammar).into_iter();
+    let last = seed_token_iter.next_back();
+
+    Ok(format!(
+        "{}{}",
+        tokens_to_string(&seed_token_iter.collect(), &token_grammar)?,
+        generate_gibberish(&last.unwrap(), &token_grammar, max_token_count)?
+    ))
+}
+
+/// Print all tokens in a token grammar.
+pub fn debug_grammar(token_grammar: &TokenGrammar) {
+    let _: Vec<_> = token_grammar
+        .iter()
+        .enumerate()
+        .inspect(|(i, (l, r, f))| {
+            println!(
+                "({} + {}) {}{} -> T{} ({})",
+                l,
+                r,
+                token_to_chars(l, token_grammar).unwrap(),
+                token_to_chars(r, token_grammar).unwrap(),
+                i,
+                f
+            )
+        })
+        .collect();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,132 +1,23 @@
 use anyhow::{Context, Result};
 use bpe_thing::*;
-use std::{
-    collections::HashMap,
-    env,
-    fs::{self},
-    path::Path,
-};
-
-// TODO: actual usable cli
-
-fn parse_bpe<P: AsRef<Path>>(
-    txt_path: P,
-    bpe_path: P,
-    max_token_count: u32,
-) -> Result<TokenGrammar> {
-    let data = fs::read_to_string(txt_path)?;
-
-    // Byte to use for non-char BPE tokens, incremented on demand.
-    let mut char_count = 0;
-
-    // Input tokens to be encoded with BPE.
-    let mut tokens: Vec<Token> = data.chars().map(|c| Token::Char(c)).collect();
-
-    // Finalised token 'grammar' definition of (left-token, right-token, frequency).
-    let mut token_grammar: TokenGrammar = vec![];
-
-    // Hashmap of {(left-token, right-token): frequency} for the entire token stream.
-    let mut freqs: HashMap<(Token, Token), usize> = HashMap::new();
-    for i in 0..tokens.len() - 1 {
-        freqs
-            .entry((tokens[i].clone(), tokens[i + 1].clone()))
-            .and_modify(|freq| *freq += 1)
-            .or_insert(1);
-    }
-
-    for _ in 0..max_token_count {
-        let (max_pair, max_freq) = freqs
-            .iter()
-            .max_by(|(_, l_freq), (_, r_freq)| l_freq.cmp(r_freq))
-            .map(|(k, v)| ((*k).clone(), *v))
-            .unwrap();
-
-        if max_freq == 1 {
-            break;
-        };
-
-        freqs.remove(&max_pair);
-        token_grammar.push((max_pair.0.clone(), max_pair.1.clone(), max_freq as u32));
-
-        let mut i = 0;
-        while i < tokens.len() - 1 {
-            if max_pair.0 == tokens[i] && max_pair.1 == tokens[i + 1] {
-                // Replace most common pair bc at index i with new token Z: abcd -> aZd
-                tokens.push(Token::Pair(char_count)); // abcdZ
-                tokens.remove(i); // acdZ
-                tokens.swap_remove(i); // aZd
-
-                if i > 0 {
-                    // Decrement ac freq, increment aZ freq or create it if it does not exist.
-                    freqs
-                        .entry((tokens[i - 1].clone(), max_pair.0.clone()))
-                        .and_modify(|freq| *freq -= 1);
-                    freqs
-                        .entry((tokens[i - 1].clone(), Token::Pair(char_count)))
-                        .and_modify(|freq| *freq += 1)
-                        .or_insert(1);
-                }
-
-                if i < tokens.len() - 2 {
-                    // Decrement cd freq, increment Zd freq or create it if it does not exist.
-                    freqs
-                        .entry((max_pair.1.clone(), tokens[i + 1].clone()))
-                        .and_modify(|freq| *freq -= 1);
-                    freqs
-                        .entry((Token::Pair(char_count), tokens[i + 1].clone()))
-                        .and_modify(|freq| *freq += 1)
-                        .or_insert(1);
-                }
-            }
-
-            i += 1;
-        }
-
-        char_count += 1;
-    }
-
-    // println!("{:?}", tokens);
-    // let _: Vec<_> = token_grammar
-    //     .iter()
-    //     .enumerate()
-    //     .inspect(|(i, (l, r, f))| println!("{}, {} -> T{} ({})", l, r, i, f))
-    //     .collect();
-
-    write_tokens(&token_grammar, bpe_path)?;
-
-    Ok(token_grammar)
-}
-
-fn generate_from_seed<P: AsRef<Path>>(
-    seed: String,
-    bpe_path: P,
-    max_token_count: u32,
-) -> Result<String> {
-    let token_grammar = load_tokens(bpe_path)?;
-
-    let mut seed_token_iter = tokenize(seed, &token_grammar).into_iter();
-    let last = seed_token_iter.next_back();
-
-    Ok(format!(
-        "{}{}",
-        tokens_to_string(&seed_token_iter.collect(), &token_grammar)?,
-        generate_gibberish(&last.unwrap(), &token_grammar, max_token_count)?
-    ))
-}
+use std::env;
 
 macro_rules! pos_args {
+    ($cmd:literal, $iter:ident, $n:literal) => {
+        {
+            let arg = $iter.next().context(format!("Please provide a value for '{}'.", $n))?;
+            if arg.as_str() == "--help" || arg.as_str() == "-h" {
+                print_help(Some($cmd));
+                return Ok(());
+            } else {
+                arg
+            }
+        }
+    };
     ($cmd:literal, $iter:ident, $($n:literal),*) => {
         (
             $(
-                {
-                    let arg = $iter.next().context(format!("Please provide a value for '{}'.", $n))?;
-                    if arg.as_str() == "--help" || arg.as_str() == "-h" {
-                        print_help(Some($cmd));
-                        return Ok(());
-                    } else {
-                        arg
-                    }
-                },
+                pos_args!($cmd, $iter, $n),
             )*
         )
     };
@@ -217,7 +108,12 @@ fn main() -> Result<()> {
                 flag_args!("parse", args_iter, (max_token_count, "--max-tokens" | "-t"));
 
                 parse_bpe(txt_path, bpe_path, max_token_count)?;
-                return Ok(());
+            }
+            "debug" | "d" => {
+                let bpe_path = pos_args!("debug", args_iter, "bpe_path");
+
+                let token_grammar = load_tokens(bpe_path)?;
+                debug_grammar(&token_grammar);
             }
             "--help" | "-h" => print_help(None),
             unknown => {
